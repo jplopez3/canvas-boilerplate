243e673108f7e527b88e79704402414f00423519
João Lopez <jp-lopez@hotmail.com>
2019-03-20 11:51:05 +0000

    add gulp file

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sync = require('browser-sync');
+
+gulp.task('serve', function() {
+  sync({
+    server: {
+      baseDir: './',
+      // index: 'outracoisa.html' se fosse outro nome q nao index
+    },
+    port: 8000
+  });
+  gulp.watch('*.html').on('change', sync.reload);
+  refresh.listen();
+  gulp.watch('styles/*.scss', ['sassing', sync.reload]);
+  gulp.watch('scripts/*.js', ['scripting', sync.reload]);
+
+});
+
+
+gulp.task('default', ['serve']);
